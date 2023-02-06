be9c5665dcbcfde53d3a31a254997254ed6ee42a
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-02-07 00:52:35 +0530

    rotate

--- capture.py
+++ capture.py
@@ -5,24 +5,16 @@ cam = cv2.VideoCapture(0)
 path = "./rps/scissor/"
 
 time.sleep(2)
-ret,frame = cam.read()
-for i in range(1,501):
-    ret,frame = cam.read()
-    # frame = cv2.resize(frame,(500,500))
+ret, frame = cam.read()
 
+for i in range(1, 501):
+    ret,frame = cam.read()
 
     rz = 1080
-    x = frame.shape[1] // 2 - (rz//2)
-    y = frame.shape[0] // 2 - (rz//2)
+    x = (frame.shape[1] // 2) - (rz // 2)
+    y = (frame.shape[0] // 2) - (rz // 2)
     
-    # print(frame.shape[1])
-    # print(frame.shape[0])
-    
-
-
-    
-    frame = frame[y:y+rz,x:x+rz]
-    cv2.imshow("jatere",frame)
-    cv2.imwrite((path+str(i)+".jpeg"),frame)
+    frame = frame[y:y+rz, x:x+rz]
+    cv2.imwrite((path + str(i)+ ".jpeg"), frame)
     time.sleep(.5)
 
---
+++ rotate.py
@@ -0,0 +1,12 @@
+import cv2
+
+path = "./rps/paper/"
+
+for i in range(1,2):
+    frame = cv2.imread(path + str(i) + ".jpeg")
+
+    cv2.imwrite((path + str(i) + "-90.jpeg"), cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE))
+
+    cv2.imwrite((path + str(i) + "-180.jpeg"), cv2.rotate(frame, cv2.ROTATE_180))
+
+    cv2.imwrite((path + str(i) + "-270.jpeg"), cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE))
